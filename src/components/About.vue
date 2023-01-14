5aae2fa3c21c13fa1b295fdb832836e5c6358080
Reece1109 <reecesimon84@gmail.com>
2023-01-14 09:35:03 +0200

    First commit

---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<template >
+  <div>
+    <h1> About Page </h1> 
+      <p>My name is Reece Simon and I am a open-minded, goal driven and an aspiring Developer.
+       I am always looking for new opportunities to grow and acquire new skills and tools. 
+       I see myself as a asset to any workspace because of my work ethic, perseverance and drive for success .
+       I am really interested in the Cyber Security part of coding. As I am all about safeguarding and protecting privileged information and theft.
+          1999/09/11
+          067 743 2578</p>
+  </div>
+</template>
+<script>
+import {  } from 'vue'
+export default {
+  
+}
+</script>
+<style>
+  
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<template>
+<!-- Nav-Bar -->
+  <nav>
+<nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Page</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Work & Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Testimonials</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Page</a>
+          </li>
+          <li class="nav-item dropdown">
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+  </nav>
+  <div class="hello">
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+ 
